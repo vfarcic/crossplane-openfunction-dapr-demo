89334e9f123430873fb6268c9b56956e7bec1c80
Viktor Farcic <viktor@farcic.com>
2023-10-22 18:56:02 +0200

    Initial commit

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package sillydemo
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-
-	ofctx "github.com/OpenFunction/functions-framework-go/context"
 
 	"github.com/OpenFunction/functions-framework-go/functions"
 )
@@ -18,14 +15,9 @@ func init() {
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	response := map[string]string{
-		"output": "This is a silly demo",
+		"output": "This is a silly demo!!!",
 	}
 	responseBytes, _ := json.Marshal(response)
 	w.Header().Set("Content-type", "application/json")
 	w.Write(responseBytes)
-}
-
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	vars := ofctx.VarsFromCtx(r.Context())
-	fmt.Fprintf(w, "Hello, %s!\n", vars["greeting"])
 }
